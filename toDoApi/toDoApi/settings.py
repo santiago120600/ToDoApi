09f529bf4a27e82394e455325caca386ea98184c
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-02-01 18:50:18 -0600

    enabled CORS

--- toDoApi/toDoApi/settings.py
+++ toDoApi/toDoApi/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "api",
+    "corsheaders",
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST  = [
+        "http://localhost:3000"
+        ]
